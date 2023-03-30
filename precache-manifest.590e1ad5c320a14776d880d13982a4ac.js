feb2f6e55cfce2ea257f97005cd2da07ebf5a418
Schnippen <dfojcik35@gmail.com>
2023-03-31 00:26:14 +0200

    Updates

---
+++ precache-manifest.590e1ad5c320a14776d880d13982a4ac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "14aa95fa94408e3fc4c9",
+    "url": "/static/css/main.8f04f3bb.chunk.css"
+  },
+  {
+    "revision": "14aa95fa94408e3fc4c9",
+    "url": "/static/js/main.14aa95fa.chunk.js"
+  },
+  {
+    "revision": "2981538dd718ef902032",
+    "url": "/static/js/1.2981538d.chunk.js"
+  },
+  {
+    "revision": "69f82cf6053aae2300ec",
+    "url": "/static/js/2.69f82cf6.chunk.js"
+  },
+  {
+    "revision": "7855737d81de59457bd8",
+    "url": "/static/js/runtime~main.7855737d.js"
+  },
+  {
+    "revision": "41613602d5b2838640efdab9edb24cb9",
+    "url": "/index.html"
+  }
+];
