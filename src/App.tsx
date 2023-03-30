239ccce621b1b7f6d6c2af1b0ea1618f668260aa
Schnippen <dfojcik35@gmail.com>
2023-03-30 16:41:10 +0200

    modified:   package-lock.json 	modified:   package.json 	renamed:    src/App.js -> src/App.tsx 	new file:   src/components/Navbar/Navbar.module.css 	new file:   src/components/Navbar/Navbar.tsx 	modified:   src/index.css 	renamed:    src/index.js -> src/index.tsx 	new file:   src/react-app-env.d.ts 	new file:   styles.d.ts 	new file:   tsconfig.json

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import React from "react";
+import "./App.css";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <div className="App">
+      <Navbar></Navbar>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./Navbar.module.css"
+
+function Navbar() {
+  return (
+    <ul className={styles.navbar}>
+      <li></li>
+      <li>logo</li>
+      <li></li>
+    </ul>
+  );
+}
+
+export default Navbar;
